2ca0b08aff77dfcd70174115f1ef38ef176f9726
mikkel <mikkellars@hotmail.com>
2019-11-27 13:57:02 +0100

    solvePnP not give correct result

--- VIS/SparseStereo/src/SparseStereo.cpp
+++ VIS/SparseStereo/src/SparseStereo.cpp
@@ -11,7 +11,7 @@
 #include <rwslibs/rwstudioapp/RobWorkStudioApp.hpp>
 
 #include <iostream>
-
+#include <fstream>
 
 struct Camera {
     cv::Mat intrinsic;
@@ -30,7 +30,7 @@ struct Camera {
     }
 };
 
-void printProjectionMatrix(std::string frameName, rw::models::WorkCell::Ptr wc, cv::Mat &proj_mat) {
+void printProjectionMatrix(std::string frameName, rw::models::WorkCell::Ptr wc, cv::Mat &proj_mat, cv::Mat &cam_mat) {
     rw::kinematics::State state = wc->getDefaultState();
     rw::kinematics::Frame* cameraFrame = wc->findFrame(frameName);
     if (cameraFrame != NULL) {
@@ -53,8 +53,8 @@ void printProjectionMatrix(std::string frameName, rw::models::WorkCell::Ptr wc, 
             cv::Mat KA_opencv = (cv::Mat_<double>(3, 4) << fovy_pixel, 0, width/2.0, 0,
                                                            0, fovy_pixel, height / 2.0, 0,
                                                            0, 0, 1, 0);
-            std::cout << "test " << KA_opencv << std::endl;
 
+            cam_mat = KA_opencv.colRange(0, 3);
 
             std::cout << "Intrinsic parameters:" << std::endl;
             std::cout << KA << std::endl;
@@ -110,6 +110,19 @@ cv::Mat color_threshold_duck(cv::Mat pic){
 
 }
 
+void write_transformation_mat(cv::Mat rotation_mat, cv::Mat translate_vec){
+    std::ofstream outfile2;
+
+    outfile2.open("M3_pose_estimate.txt");
+
+    outfile2 << rotation_mat.at<double>(0, 0) << " " << rotation_mat.at<double>(0, 1) << " " << rotation_mat.at<double>(0, 2) << " " << translate_vec.at<double>(0, 0) << std::endl;
+    outfile2 << rotation_mat.at<double>(1, 0) << " " << rotation_mat.at<double>(1, 1) << " " << rotation_mat.at<double>(1, 2) << " " << translate_vec.at<double>(1, 0) << std::endl;
+    outfile2 << rotation_mat.at<double>(2, 0) << " " << rotation_mat.at<double>(2, 1) << " " << rotation_mat.at<double>(2, 2) << " " << translate_vec.at<double>(2, 0) << std::endl;
+    outfile2 <<          0          << " " <<          0          << " " <<          0          << " " <<      1    << std::endl;
+    outfile2.close();
+
+}
+
 int main()
 {
     // Load workcell
@@ -127,9 +140,9 @@ int main()
 //        return -1;
 //    }
     // Gets projection matrix for both cameras
-    cv::Mat projection_mat_left, projection_mat_right;
-    printProjectionMatrix("Camera_Left", wc, projection_mat_left);
-    printProjectionMatrix("Camera_Right", wc, projection_mat_right);
+    cv::Mat projection_mat_left, projection_mat_right, cam_left_mat, cam_right_mat;
+    printProjectionMatrix("Camera_Left", wc, projection_mat_left, cam_left_mat);
+    printProjectionMatrix("Camera_Right", wc, projection_mat_right, cam_right_mat);
 //    std::cout << "Proj Left: " << projection_mat_left << std::endl;
 //    std::cout << "Proj Right: " << projection_mat_right << std::endl;
 
@@ -161,14 +174,13 @@ int main()
     std::vector<std::vector<cv::DMatch>> knn_matches;
     matcher->knnMatch( descriptor_left, descriptor_right, knn_matches, 2 );
 
-    const float ratio_thresh = 0.3f;
+    const float ratio_thresh = 0.7f;
     std::vector<cv::DMatch> good_matches;
     for (size_t i = 0; i < knn_matches.size(); i++)
     {
         if (knn_matches[i][0].distance < ratio_thresh * knn_matches[i][1].distance)
         {
             good_matches.push_back(knn_matches[i][0]);
-
         }
     }
 
@@ -182,34 +194,84 @@ int main()
     /****************************************************************************************
      **                   Triangulates the found features mathces                          **
      ****************************************************************************************/
-/*
-    cv::Mat triangulate_point(1, 1, CV_64FC4);
-    cv::Mat left_point(1, 1, CV_64FC2);
-    cv::Mat right_point(1, 1, CV_64FC2);
 
-    left_point.at<cv::Vec2d>(0)[0] = keypoint_left[1].pt.x;
-    left_point.at<cv::Vec2d>(0)[1] = keypoint_left[1].pt.y;
-    right_point.at<cv::Vec2d>(0)[0] = keypoint_right[1].pt.x;
-    right_point.at<cv::Vec2d>(0)[1] = keypoint_right[1].pt.y;
-
-    cv::triangulatePoints(projection_mat_left, projection_mat_right, left_point, right_point, triangulate_point);
-
-    std::cout << triangulate_point << std::endl;
-*/
     std::vector<cv::KeyPoint> good_keypoint_left, good_keypoint_right;
     for(int i = 0; i < good_matches.size(); i++)
     {
         good_keypoint_left.push_back(keypoint_left[good_matches[i].queryIdx]);
-        good_keypoint_right.push_back(keypoint_right[good_matches[i].queryIdx]);
-        //std::cout << good_matches[i].queryIdx << std::endl;
+        good_keypoint_right.push_back(keypoint_right[good_matches[i].trainIdx]);
         std::cout << "Left " << good_keypoint_left[i].pt << std::endl;
         std::cout << "Right " <<good_keypoint_right[i].pt << std::endl;
     }
-    std::cout <<
+
     cv::drawMatches(pic_left, keypoint_left, pic_right, keypoint_right, good_matches, result, cv::Scalar::all(-1),
             cv::Scalar::all(-1), std::vector<char>(), cv::DrawMatchesFlags::NOT_DRAW_SINGLE_POINTS);
     cv::imshow("sift matching result", result);
     cv::waitKey(0);
+//    cv::waitKey(0);
+//    cv::Point2f t1(good_keypoint_left[0].pt.x, good_keypoint_left[0].pt.y);
+//    cv::Point2f t2(good_keypoint_left[1].pt.x, good_keypoint_left[1].pt.y);
+//    cv::Point2f t3(good_keypoint_left[2].pt.x, good_keypoint_left[2].pt.y);
+//    cv::circle(pic_left, t1, 2, cv::Scalar(0, 0, 255), 2);
+//    cv::circle(pic_left, t2, 2, cv::Scalar(255, 0, 0), 2);
+//    cv::circle(pic_left, t3, 2, cv::Scalar(255, 255, 0), 2);
+//    cv::imshow("Left", pic_left);
+
+//    cv::Point2f st1(good_keypoint_right[0].pt.x, good_keypoint_right[0].pt.y);
+//    cv::Point2f st2(good_keypoint_right[1].pt.x, good_keypoint_right[1].pt.y);
+//    cv::Point2f st3(good_keypoint_right[2].pt.x, good_keypoint_right[2].pt.y);
+//    cv::circle(pic_right, st1, 2, cv::Scalar(0, 0, 255), 2);
+//    cv::circle(pic_right, st2, 2, cv::Scalar(255, 0, 0), 2);
+//    cv::circle(pic_right, st3, 2, cv::Scalar(255, 255, 0), 2);
+//    cv::imshow("Rigth", pic_right);
+//      cv::waitKey(0);
+
+
+    cv::Mat triangulate_point(1, good_matches.size(), CV_64FC4);
+    cv::Mat left_point(1, good_matches.size(), CV_64FC2);
+    cv::Mat right_point(1, good_matches.size(), CV_64FC2);
+
+    for(int i = 0; i < good_matches.size(); i++)
+    {
+        left_point.at<cv::Vec2d>(i)[0] = good_keypoint_left[i].pt.x;
+        left_point.at<cv::Vec2d>(i)[1] = good_keypoint_left[i].pt.y;
+
+        right_point.at<cv::Vec2d>(i)[0] = good_keypoint_right[i].pt.x;
+        right_point.at<cv::Vec2d>(i)[1] = good_keypoint_right[i].pt.y;
+
+    }
+
+    cv::triangulatePoints(projection_mat_left, projection_mat_right, left_point, right_point, triangulate_point);
+
+    cv::Mat dist_coeffs = cv::Mat::zeros(4,1,cv::DataType<double>::type); // Assuming no lens distortion
+    cv::Mat rotation_vector; // Rotation in axis-angle form
+    cv::Mat translation_vector;
+
+    std::vector<cv::Point2d> image_points;
+
+    std::vector<cv::Point3d> world_points;
+    cv::Mat world_p_norm_mat;
+
+    for(int i = 0; i < left_point.cols; i++)
+    {
+        image_points.push_back(left_point.at<cv::Vec2d>(i));
+        world_p_norm_mat = triangulate_point.col(i).rowRange(0, 3) / triangulate_point.at<double>(3, i); // Normalizes the 3d points
+        cv::Point3d world_p_norm( world_p_norm_mat.at<double>(0, 0), world_p_norm_mat.at<double>(0, 1), world_p_norm_mat.at<double>(0, 2));
+        world_points.push_back(world_p_norm);
+    }
+
+
+    std::cout << world_points[0] << std::endl;
+
+    //cv::solvePnP(world_points, left_point, cam_left_mat, dist_coeffs, rotation_vector, translation_vector);
+    cv::solvePnPRansac(world_points, left_point, cam_left_mat, dist_coeffs, rotation_vector, translation_vector);
+    cv::Mat rotation_mat;
+    cv::Rodrigues(rotation_vector, rotation_mat);
+    std::cout << "Rotation vector" << std::endl << rotation_mat << std::endl;
+
+    std::cout << "Translation vector" << std::endl << translation_vector << std::endl;
+
+    write_transformation_mat(rotation_mat, translation_vector);
 
     return 0;
 }
